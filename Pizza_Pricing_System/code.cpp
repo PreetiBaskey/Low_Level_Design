f67ab871118a101def7252cdf2c56b2161de8ca8
PreetiBaskey <38905783+PreetiBaskey@users.noreply.github.com>
2026-03-04 12:23:24 +0530

    new updation added

---
+++ Pizza_Pricing_System/code.cpp
@@ -0,0 +1,22 @@
+/*
+  Requirements : 
+  1. Initialize pizzas (base pizzas)
+  2. Add toppings
+  3. Calculate final price
+  4. Support business rules
+    i.   Discounts
+    ii.  Mutually exclusive toppings
+    iii. Possibly combo offers
+  5. System should be extensible
+
+  High-Level Architecture
+  1. Core Entities
+  2. Pricing Engine
+  3. Rules Engine
+  4. Decorators for Toppings
+*/
+
+int main() {
+
+  return 0;
+}
